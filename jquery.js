393511d2b9111f8ed67833f25cbbc8519d53e264
Mohammed Abdulkereem <HorizonDragon@outlook.com>
2018-08-13 11:26:18 +0300

    Added extend and each()

--- jquery.js
+++ jquery.js
@@ -1,5 +1,29 @@
-function $() {
+function $(param) {
     if (!(this instanceof $)) {
-        return new $();
+        return new $(param);
+    }
+
+    if (typeof param === 'string') {
+        Array.prototype.push.apply(this, document.querySelectorAll(param));
+    }
+
+    if (param instanceof HTMLElement) {
+        Array.prototype.push.call(this, param);
     }
 }
+
+$.extend = function(destination, source) {
+    for(let prop in source) {
+        destination[prop] = source[prop];
+    }
+    return destination;
+}
+
+$.extend($.prototype, {
+    each: function(callback) {
+        Array.prototype.forEach.call(this, (x, i) => {
+            callback(i,x);
+        })
+        return this;
+    }
+});
